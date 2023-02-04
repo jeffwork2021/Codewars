b1fd1024d20c0889ff4aec13d2acb22d5cceaf8a
Jeff Work <jeffwork2021@gmail.com>
2023-02-04 12:13:28 +0800

    Added Beginner Series #3 Sum of Numbers

---
+++ Beginner Series #3 Sum of Numbers.py
@@ -0,0 +1,30 @@
+# Given two integers a and b, which can be positive or negative, find the sum 
+# of all the integers between and including them and return it. If the two 
+# numbers are equal return a or b.
+# 
+# Note: a and b are not ordered!
+# Examples (a, b) --> output (explanation)
+# 
+# (1, 0) --> 1 (1 + 0 = 1)
+# (1, 2) --> 3 (1 + 2 = 3)
+# (0, 1) --> 1 (0 + 1 = 1)
+# (1, 1) --> 1 (1 since both are same)
+# (-1, 0) --> -1 (-1 + 0 = -1)
+# (-1, 2) --> 2 (-1 + 0 + 1 + 2 = 2)
+# 
+# Your function should only return a number, not the explanation about how you 
+# get that number.
+def get_sum(a,b):
+    step = -1 if a > b else 1
+    sum = 0
+    for i in range(a, b+step, step):
+        sum += i
+    return sum
+
+print(get_sum(1, 0))
+print(get_sum(1, 2))
+print(get_sum(0, 1))
+print(get_sum(1, 1))
+print(get_sum(-1, 0))
+print(get_sum(-1, 2))
+
