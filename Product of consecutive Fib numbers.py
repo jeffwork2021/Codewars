2b996457ef0c4c23495dd1d9c702495d268e59d5
Jeff Work <jeffwork2021@gmail.com>
2023-02-18 23:20:44 +0800

    Added Product of consecutive Fib numbers

---
+++ Product of consecutive Fib numbers.py
@@ -0,0 +1,75 @@
+# The Fibonacci numbers are the numbers in the following integer sequence (Fn):
+# 
+#     0, 1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89, 144, 233, ...
+# 
+# such as
+# 
+#     F(n) = F(n-1) + F(n-2) with F(0) = 0 and F(1) = 1.
+# 
+# Given a number, say prod (for product), we search two Fibonacci numbers F(n) 
+# and F(n+1) verifying
+# 
+#     F(n) * F(n+1) = prod.
+# 
+# Your function productFib takes an integer (prod) and returns an array:
+# 
+# [F(n), F(n+1), true] or {F(n), F(n+1), 1} or (F(n), F(n+1), True)
+# 
+# depending on the language if F(n) * F(n+1) = prod.
+# 
+# If you don't find two consecutive F(n) verifying F(n) * F(n+1) = prodyou will 
+# return
+# 
+# [F(n), F(n+1), false] or {F(n), F(n+1), 0} or (F(n), F(n+1), False)
+# 
+# F(n) being the smallest one such as F(n) * F(n+1) > prod.
+# Some Examples of Return:
+# 
+# (depend on the language)
+# 
+# productFib(714) # should return (21, 34, true), 
+#                 # since F(8) = 21, F(9) = 34 and 714 = 21 * 34
+# 
+# productFib(800) # should return (34, 55, false), 
+#                 # since F(8) = 21, F(9) = 34, F(10) = 55 and 21 * 34 < 800 < 
+#                   34 * 55
+# -----
+# productFib(714) # should return [21, 34, true], 
+# productFib(800) # should return [34, 55, false], 
+# -----
+# productFib(714) # should return {21, 34, 1}, 
+# productFib(800) # should return {34, 55, 0},        
+# -----
+# productFib(714) # should return {21, 34, true}, 
+# productFib(800) # should return {34, 55, false}, 
+# 
+# Note:
+# 
+#     You can see examples for your language in "Sample Tests".
+# 
+def productFib(prod):
+    # your code
+    loop_switch = True
+    n = 0
+    last_value_n = 0
+    last_last_value_n = 0
+    value_n = 0
+    while loop_switch:
+        last_last_value_n = last_value_n
+        last_value_n = value_n
+        if n > 1:
+            value_n = last_value_n + last_last_value_n
+        elif n == 1:
+            value_n = 1
+        elif n == 0:
+            value_n = 0
+
+        value_prod = last_value_n * value_n
+        if value_prod >= prod and n > 0:
+            return [last_value_n, value_n, value_prod == prod]
+
+        n += 1
+        
+print(productFib(714))
+print(productFib(800))
+print(productFib(0))
