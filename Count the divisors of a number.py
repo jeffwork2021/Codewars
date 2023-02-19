2699ce5ad85be0b51eb25353fe8c3bd390a66ed6
Jeff Work <jeffwork2021@gmail.com>
2023-02-19 16:49:27 +0800

    Added Count the divisors of a number

---
+++ Count the divisors of a number.py
@@ -0,0 +1,26 @@
+# Count the number of divisors of a positive integer n.
+# 
+# Random tests go up to n = 500000.
+# Examples (input --> output)
+# 
+# 4 --> 3 (1, 2, 4)
+# 5 --> 2 (1, 5)
+# 12 --> 6 (1, 2, 3, 4, 6, 12)
+# 30 --> 8 (1, 2, 3, 5, 6, 10, 15, 30)
+# 
+# Note you should only return a number, the count of divisors. The numbers 
+# between parentheses are shown only for you to see which numbers are counted 
+# in each case.
+def divisors(n):
+    i = 1
+    count = 1
+    while i < n:
+        if n % i == 0:
+            count += 1
+        i += 1
+    return count
+
+print(divisors(4))
+print(divisors(5))
+print(divisors(12))
+print(divisors(30))
