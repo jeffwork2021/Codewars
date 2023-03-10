ae5a9e4486cdb51ed7b63b20250ce3e83ff38b2a
jeffwork2021 <jeffwork2021@gmail.com>
2023-03-10 13:52:14 +0800

    Added Removing Elements

---
+++ Removing Elements.py
@@ -0,0 +1,14 @@
+# Take an array and remove every second element from the array. Always keep the 
+# first element and start removing with the next element.
+# Example:
+#
+# ["Keep", "Remove", "Keep", "Remove", "Keep", ...] --> ["Keep", "Keep", 
+# "Keep", ...]
+#
+# None of the arrays will be empty, so you don't have to worry about that!
+def remove_every_other(my_list):
+    return my_list[::2]
+
+print(remove_every_other(["Keep", "Remove", 
+                          "Keep", "Remove", 
+                          "Keep", "Remove"]))
